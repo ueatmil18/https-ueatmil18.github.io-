83fbe7ef36a8e7c27baaba1b5c1ba4fa569580b1
JJ Alvarez <ueatm2012@gmail.com>
2022-11-13 17:11:04 -0300

    13/11/22

--- modulo2/models/UI.js
+++ modulo2/models/UI.js
@@ -30,20 +30,17 @@ export class UI {
 
   showScores(score) {
     var ImgScore="";
-    if(score<3 || score ==3) {
+    if(score<3 || score ==3)
       ImgScore="florkLoca.jpg";
-    }else if(score>3 || score<7){
+    else if(score>3 || score<7)
       ImgScore="florkOk.jpg";
-    }else {
-      ImgScore="florkNice.jpg";
-    }
-
-    ImgScore="florkNice.jpg";
+    else ImgScore="florkNice.jpg";
+    
     console.log("img:"+ImgScore);
     const gameOverHTML = `
       <h1>Resultados</h1>
       <h2 id="score">Tus Puntos: ${score}</h2>
-      <img style="width:100px;height:100px;" src="imgs/${ImgScore}">`;
+      <img style="width:200px;height:200px;" src="imgs/${ImgScore}">`;
    
     const element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
